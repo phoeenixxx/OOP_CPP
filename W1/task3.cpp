bc1e6e2e36424f8e8e3a5f26176a7b8a4b0242a0
Katherin <155001524+phoeenixxx@users.noreply.github.com>
2026-04-02 11:53:45 +0400

    Add C program to print alphabets using pointers

---
+++ W1/task3.cpp
@@ -0,0 +1,35 @@
+// =================================================================
+/*
+ * Exercise 3:
+ * Write a program in C to print all the alphabets using pointers. [cite: 7]
+ */
+// =================================================================
+#include <stdio.h>
+
+int main() {
+    char alph[27];
+    int x;
+    char *ptr;
+    
+    printf("\n\n Pointer : Print all the alphabets:\n");
+    printf("----------------------------------------\n");
+    
+    ptr = alph;
+    
+    for(x = 0; x < 26; x++) {
+        *ptr = x + 'A';
+        ptr++;
+    }
+    
+    ptr = alph;
+    
+    printf(" The Alphabets are : \n");
+    
+    for(x = 0; x < 26; x++) {
+        printf(" %c ", *ptr);
+        ptr++;
+    }
+    
+    printf("\n\n");
+    return 0;
+}
